283b52064e94d21913c3ff1dc5305f26746c6121
rayleighko <ray@plan-cy.com>
2022-12-10 15:45:22 +0900

    feat: Add Header and Modify Introduction section(style)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,26 @@
 import "twin.macro"
 
+import AdbIcon from "@mui/icons-material/Adb"
+import MenuIcon from "@mui/icons-material/Menu"
 import { Typography, Unstable_Grid2 } from "@mui/material"
+import AppBar from "@mui/material/AppBar"
+import Avatar from "@mui/material/Avatar"
+import Box from "@mui/material/Box"
+import Button from "@mui/material/Button"
+import Container from "@mui/material/Container"
+import IconButton from "@mui/material/IconButton"
+import Menu from "@mui/material/Menu"
+import MenuItem from "@mui/material/MenuItem"
+import Toolbar from "@mui/material/Toolbar"
+import Tooltip from "@mui/material/Tooltip"
 import { CryptoCurrencyCard } from "components/card/CryptoCurrencyCard"
 import { CRYPTO_CURRENCY_SYMBOL } from "components/icon/CryptoCurrencyIcon/constants"
 import { CryptoCurrencySymbol } from "components/icon/CryptoCurrencyIcon/type"
-import { FC, PropsWithChildren } from "react"
+import { FC, MouseEvent, PropsWithChildren, useState } from "react"
 import { genUniqueRandomNumArr } from "utils/common"
+
+const pages = ["Products", "Pricing", "Blog"]
+const settings = ["Profile", "Account", "Dashboard", "Logout"]
 
 const majorCryptoCurrenciesTop4: CryptoCurrencySymbol[] = [
   "BTC",
@@ -26,7 +41,7 @@ const IntroductionSection = () => {
 
   return (
     <section tw="w-full flex items-center bg-gray-100 py-8">
-      <div tw="w-1/2 items-center">
+      <div tw="w-full items-center xl:w-1/2">
         <Typography
           tw="text-center font-extrabold text-4xl"
           variant="h2"
@@ -34,7 +49,7 @@ const IntroductionSection = () => {
           The Decentralized Application List
         </Typography>
       </div>
-      <div tw="w-1/2 items-center">
+      <div tw="w-full items-center hidden xl:block xl:w-1/2">
         <Unstable_Grid2 container spacing={2} tw="flex justify-center">
           {majorCryptoCurrenciesTop4.map((symbol) => (
             <Unstable_Grid2 key={symbol} tw="flex justify-center">
@@ -97,7 +112,7 @@ const PricingSection = () => {
 }
 const SubscribeSection = () => {
   return (
-    <section>
+    <section tw="w-full flex items-center bg-white py-8">
       <Unstable_Grid2 container tw="w-full flex-col items-center">
         <Unstable_Grid2 tw="pb-0 text-center text-2xl" xs={4} sm={4} md={4}>
           SubscribeSection
@@ -108,14 +123,132 @@ const SubscribeSection = () => {
 }
 
 const Header = () => {
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null)
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null)
+
+  const handleOpenNavMenu = (event: MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget)
+  }
+  const handleOpenUserMenu = (event: MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget)
+  }
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null)
+  }
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null)
+  }
+
   return (
-    <header>
-      <Unstable_Grid2 container tw="w-full flex-col items-center">
-        <Unstable_Grid2 tw="pb-0 text-center text-2xl" xs={4} sm={4} md={4}>
-          Header
-        </Unstable_Grid2>
-      </Unstable_Grid2>
-    </header>
+    <AppBar position="static" tw="bg-white">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h4"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}>
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit">
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}>
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            tw="mr-2 flex-grow font-mono no-underline"
+            sx={{
+              display: { xs: "flex", md: "none" },
+            }}>
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Tooltip key={page} title="Coming soon">
+                <Button
+                  onClick={handleCloseNavMenu}
+                  tw="my-2 text-black block text-sm">
+                  {page}
+                </Button>
+              </Tooltip>
+            ))}
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Coming soon">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              // open={Boolean(anchorElUser)}
+              open={false}
+              onClose={handleCloseUserMenu}>
+              {/* {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))} */}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   )
 }
 const Footer = () => {
